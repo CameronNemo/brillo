b59db4c5c734a7d6f52bf40375fe7e01ee84661e
Marius <froase@gmail.com>
2018-10-10 12:25:34 -0700

    file_write_smooth: account for write time, avoid extraneous writes
    Signed-off-by: Cameron Nemo <camerontnorman@gmail.com>

--- src/file.c
+++ src/file.c
@@ -11,8 +11,95 @@
 
 #define FILE_MODE_DEFAULT (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
+#define SMOOTH_WRITES_PER_SECOND 100
+#define SMOOTH_ITER_DURATION 1e9 / SMOOTH_WRITES_PER_SECOND
+
 /**
- * file_write_smooth:
+ * file_write_sleep:
+ * @nsec:	nanoseconds to sleep for
+ * @t0:		initial time spec to compare to
+ *
+ * Sleeps for nsec nanoseconds, subtracted by how much time
+ * has passed since the time stored in t0.
+ *
+ * Returns: true on success, false on failure
+ **/
+static bool file_write_sleep(long nsec, struct timespec t0)
+{
+	struct timespec t_sleep, t1;
+
+	t_sleep.tv_sec = 0;
+	t_sleep.tv_nsec = nsec;
+
+	/* get current time so that we can compare to t0 */
+	errno = 0;
+	if (clock_gettime(CLOCK_MONOTONIC_RAW, &t1) < 0) {
+		LIGHT_ERR("clock_gettime: %s", strerror(errno));
+		errno = 0;
+		return false;
+	}
+
+	if ((t1.tv_sec - t0.tv_sec) > 1) {
+		LIGHT_WARN("time diff greater than 1 second, skipping sleep");
+		return true;
+	}
+
+	if ((t1.tv_nsec - t0.tv_nsec) < 0)
+		t_sleep.tv_nsec -= (t1.tv_nsec - t0.tv_nsec + 1e9);
+	else
+		t_sleep.tv_nsec -= (t1.tv_nsec - t0.tv_nsec);
+
+	if (t_sleep.tv_nsec <= 0)
+		return true;
+
+	/* don't overload nanosleep() */
+	if (t_sleep.tv_nsec >= 1e9)
+		t_sleep.tv_nsec = 1e9 - 1;
+
+	errno = 0;
+	if (nanosleep(&t_sleep, NULL) < 0) {
+		LIGHT_ERR("nanosleep: %s", strerror(errno));
+		errno = 0;
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * file_rewrite:
+ * @fd:		file descriptor to write to
+ * @val:	value to write into file
+ *
+ * Truncates the file described by fd and prints val into it.
+ *
+ * Returns: true on success, false on failure
+ **/
+static bool file_rewrite(int fd, int64_t val)
+{
+	if (val < 0)
+		val = 0;
+
+	errno = 0;
+	if (ftruncate(fd, 0) < 0) {
+		LIGHT_ERR("ftruncate: %s", strerror(errno));
+		errno = 0;
+		return false;
+	}
+
+	if (dprintf(fd, "%" PRId64, val) < 0) {
+		LIGHT_ERR("dprintf: %" PRId64, val);
+		return false;
+	}
+
+	/* flush all data to disk so the change takes effect */
+	fflush(NULL);
+
+	return true;
+}
+
+/**
+ * file_write:
  * @fd:		file descriptor to write to
  * @start:	starting value
  * @end:	value to eventually write
@@ -25,45 +112,28 @@
  **/
 static bool file_write_smooth(int fd, int64_t start, int64_t end, int64_t usec)
 {
-	struct timespec tspec;
+	struct timespec t0;
 
-	tspec.tv_sec = 0;
-	tspec.tv_nsec = 1000;
+	int64_t num_writes = usec * SMOOTH_WRITES_PER_SECOND / 1e6;
 
-	for (int64_t i = 0; i <= usec; i++) {
+	for (int64_t i = 0; i <= num_writes; i++) {
 		int64_t next_value;
 
-		if (usec == 0)
+		/* save current time to account for the time
+		 * taken to perform the write operation */
+		clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+
+		if (usec == 0 || i == num_writes)
 			next_value = end;
 		else
-			next_value = ((start * usec) + ((end - start) * i)) / usec;
+			next_value = ((start * num_writes) + ((end - start) * i)) / num_writes;
 
-		if (next_value < 0)
-			next_value = 0;
-
-		errno = 0;
-		if (ftruncate(fd, 0) < 0) {
-			LIGHT_ERR("ftruncate: %s", strerror(errno));
-			errno = 0;
+		if (!file_rewrite(fd, next_value)) {
 			close(fd);
 			return false;
 		}
 
-		if (dprintf(fd, "%" PRId64, next_value) < 0) {
-			LIGHT_ERR("dprintf: %" PRId64, next_value);
-			close(fd);
-			return false;
-		}
-
-		fflush(NULL);
-
-		if (usec == 0)
-			continue;
-
-		errno = 0;
-		if (nanosleep(&tspec, NULL) < 0) {
-			LIGHT_ERR("nanosleep: %s", strerror(errno));
-			errno = 0;
+		if (!file_write_sleep(SMOOTH_ITER_DURATION, t0)) {
 			close(fd);
 			return false;
 		}
@@ -73,16 +143,13 @@ static bool file_write_smooth(int fd, int64_t start, int64_t end, int64_t usec)
 }
 
 /**
- * file_write:
- * @path:	path to write value to
- * @start:	current base value
- * @end:	value to eventually write
- * @usec:	time in microseconds to take to write the value
+ * file_open:
+ * @path:	path to open
+ * @mode:	access mode to pass to open()
  *
- * Opens a given path, obtains a lock for the file, then writes into the path.
- * Uses a smooth scale if start and end are not equal and usec is non-zero.
+ * Opens a given path and obtains a lock for the file.
  *
- * Returns: true on success, false on failure
+ * Returns: an fd for the path on success, -1 on failure
  **/
 bool file_write(char const *path, int64_t start, int64_t end, int64_t usec)
 {
